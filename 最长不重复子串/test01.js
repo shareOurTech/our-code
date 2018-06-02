3056acfb1ce8444a9a9740c3cf3b3ae8215dbda9
liuhaoran6 <liuhaoran6@jd.com>
2018-06-02 17:19:49 +0800

    MOD:最长不重复子串

--- 最长不重复子串/test01.js
+++ 最长不重复子串/test01.js
@@ -1,35 +1,36 @@
-let str = 'sjhiuataqaigdosagdghoiqwasfduwgbaadgsfnkjabgiqefajkbd';
-
-let arr = str.split('');
-
-let result = {
-    len: 0,
-    str: '',
-    start: 0,
-    end: 0
-}
-Array.prototype.quchong = function () {
-    let arr = [];
-    this.forEach((i) => {
-        if (arr.indexOf(i) === -1) {
-            arr.push(i);
-        }
-    })
-    return arr;
-}
-for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-        let childArr = arr.slice(i, j);
-        let afterQuchong = childArr.quchong();
-        let len = afterQuchong.length;
-        if (len === childArr.length && result.len < len) {
-            result = {
-                len: len,
-                str: afterQuchong.join(''),
-                start: i,
-                end: j
+function FindSubStr(str) {
+    Array.prototype.quchong = function () {
+        let arr = [];
+        this.forEach((i) => {
+            if (arr.indexOf(i) === -1) {
+                arr.push(i);
+            }
+        })
+        return arr;
+    }
+    let arr = str.split('');
+    let result = {
+        len: 0,
+        str: '',
+        start: 0,
+        end: 0
+    }
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+            let childArr = arr.slice(i, j);
+            let afterQuchong = childArr.quchong();
+            let len = afterQuchong.length;
+            if (len === childArr.length && result.len < len) {
+                result = {
+                    len: len,
+                    str: afterQuchong.join(''),
+                    start: i,
+                    end: j
+                }
             }
         }
     }
+    return result;
 }
-console.log(JSON.stringify(result));
+// let str = 'sjhiuataqaigdosagdghoiqwasfduwgbaadgsfnkjabgiqefajkbd';
+export default FindSubStr;
